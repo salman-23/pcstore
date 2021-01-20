d7da279d5598c3e9f207f0311773e724f412e514
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-01-20 13:56:33 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const titleStore = "Pc Store";
+const desc = "Where you will find the newest pc parts for your build";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>{titleStore}</h1>
+        </div>
+        <div>
+          <p>{desc}</p>
+        </div>
+        <div>
+          <img
+            src="https://static.techspot.com/images2/news/bigimage/2017/05/2017-05-02-image-2.jpg"
+            alt=""
+          />
+        </div>
       </header>
     </div>
   );
